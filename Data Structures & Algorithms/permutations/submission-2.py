8fcd4585481fd154b92cdc53204c13a600eb912b
Mohammed Abdulai <65478290+mo-abdulai@users.noreply.github.com>
2026-08-10 22:33:05 -0400

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        result = []
+        perms = []
+
+        def backtrack():
+            if len(perms) == len(nums):
+                result.append(perms.copy())
+                return
+            
+            for num in nums:
+                if num not in perms:
+                    perms.append(num)
+                    backtrack()
+                    perms.pop()
+                    
+        backtrack()
+        return result
+
+            
