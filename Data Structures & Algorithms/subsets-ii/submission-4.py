b916ea83c8323fbd517e344f15ef615a1db390b6
Mohammed Abdulai <65478290+mo-abdulai@users.noreply.github.com>
2026-08-11 22:20:33 -0400

    Add: subsets-ii - submission-4

---
+++ Data Structures & Algorithms/subsets-ii/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        subset = []
+        nums.sort()
+
+        def backtrack(index):
+
+            if index == len(nums):
+                result.append(subset.copy())
+                return
+            
+            subset.append(nums[index])
+            backtrack(index + 1)
+
+            subset.pop()
+
+            while index + 1 < len(nums) and nums[index] == nums[index + 1]:
+                index += 1
+            
+            backtrack(index + 1)
+        
+
+        backtrack(0)
+        return result
+            
+            
+
+
+        
+            
+    # O(2^n)
