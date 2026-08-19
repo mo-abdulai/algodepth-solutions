7dcb41439817ca82bea7443af8b77e7a626fca10
Mohammed Abdulai <65478290+mo-abdulai@users.noreply.github.com>
2026-08-18 22:27:54 -0400

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,41 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(row, col):
+            if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != "O":
+                return
+            board[row][col] = "T"
+
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+
+        # Capture unsourrounded region (O -> T)
+        for col in range(cols):
+            dfs(0, col)
+            dfs(rows - 1, col)
+
+        for row in range(rows):
+            dfs(row, 0)
+            dfs(row, cols - 1)
+        
+        
+        # capture sourrounding reging (O -> X) and (T -> O)       
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == "O":
+                    board[row][col] = "X"
+                
+                elif board[row][col] == "T":
+                    board[row][col] = "O"
+        
+
+       
+
+
+
+        
