e12dd8e8a5d640ab2e1b9041f8dbf60bdfd1f04e
Mohammed Abdulai <65478290+mo-abdulai@users.noreply.github.com>
2026-08-27 22:13:29 -0400

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        visited = set()
+        queue = deque()
+
+        def dfs(row, col):
+            if row < 0 or row >= rows or col < 0 or col >= cols or (row, col) in visited or grid[row][col] == -1:
+                return
+            visited.add((row, col))
+            queue.append([row, col])
+            
+            
+        
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 0:
+                    visited.add((row, col))
+                    queue.append([row, col])
+        dist = 0
+        while queue:
+            for _ in range(len(queue)):
+                row, col = queue.popleft()
+                grid[row][col] = dist
+                dfs(row + 1, col)
+                dfs(row - 1, col)
+                dfs(row, col + 1)
+                dfs(row, col - 1)
+            dist += 1
